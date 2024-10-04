95b5ef092d95948a821026e45335f44c4ecb3cca
Matthew Floyd <74754453+mf244@users.noreply.github.com>
2024-10-03 22:56:36 -0500

    unscrewed config flow

--- custom_components/homeassistant-todo-important/config_flow.py
+++ custom_components/homeassistant-todo-important/config_flow.py
@@ -1,59 +1,22 @@
-import logging
-import voluptuous as vol
-
 from homeassistant import config_entries
 from homeassistant.core import callback
-from .const import DOMAIN, CONF_CLIENT_ID, CONF_CLIENT_SECRET, CONF_RETURNED_URL, DEFAULT_REDIRECT_URI
-
-_LOGGER = logging.getLogger(__name__)
-
-@callback
-def configured_instances(hass):
-    """Return a set of configured instances."""
-    return set(entry.title for entry in hass.config_entries.async_entries(DOMAIN))
+import voluptuous as vol
+from .const import DOMAIN, CONF_CLIENT_ID, CONF_CLIENT_SECRET
 
 class MicrosoftToDoConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Config flow for Microsoft To Do integration."""
+    """Handle a config flow for Microsoft To Do integration."""
 
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
         if user_input is not None:
-            self.client_id = user_input[CONF_CLIENT_ID]
-            self.client_secret = user_input[CONF_CLIENT_SECRET]
-            return await self.async_step_auth()
+            return self.async_create_entry(title="Microsoft To Do", data=user_input)
 
-        # Show form to ask for client_id and client_secret
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({
                 vol.Required(CONF_CLIENT_ID): str,
                 vol.Required(CONF_CLIENT_SECRET): str
-            })
-        )
-
-    async def async_step_auth(self, user_input=None):
-        """Handle the authentication step."""
-        if user_input is not None:
-            returned_url = user_input[CONF_RETURNED_URL]
-            # Process the returned URL and exchange the authorization code for tokens
-            # Here you would extract the code from the URL and request access/refresh tokens
-
-            # Once authenticated, complete the config flow
-            return self.async_create_entry(title="Microsoft To Do", data={
-                CONF_CLIENT_ID: self.client_id,
-                CONF_CLIENT_SECRET: self.client_secret,
-                CONF_RETURNED_URL: returned_url
-            })
-
-        # Show the link to start authentication and ask for the returned URL
-        return self.async_show_form(
-            step_id="auth",
-            description_placeholders={
-                "auth_url": f"https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id={self.client_id}&response_type=code&redirect_uri={DEFAULT_REDIRECT_URI}&response_mode=query&scope=Tasks.ReadWrite offline_access"
-            },
-            data_schema=vol.Schema({
-                vol.Required(CONF_RETURNED_URL): str
             })
         )
---
+++ custom_components/homeassistant-todo-important/const.py
@@ -0,0 +1,8 @@
+DOMAIN = "homeassistant-todo-important"
+
+CONF_CLIENT_ID = "client_id"
+CONF_CLIENT_SECRET = "client_secret"
+CONF_REDIRECT_URI = "redirect_uri"
+CONF_RETURNED_URL = "returned_url"
+
+DEFAULT_REDIRECT_URI = "https://login.microsoftonline.com/common/oauth2/nativeclient"
