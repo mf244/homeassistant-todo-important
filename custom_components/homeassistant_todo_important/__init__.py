752faa5dda1c693196334131be2a240a7e2fb65c
mf244 <74754453+mf244@users.noreply.github.com>
2024-10-18 13:06:23 -0500

    changed oauth flow

--- custom_components/homeassistant_todo_important/__init__.py
+++ custom_components/homeassistant_todo_important/__init__.py
@@ -1,20 +1,24 @@
 import logging
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.helpers import config_entry_oauth2_flow
+
 from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Set up Microsoft To Do from a config entry."""
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry.data
 
-    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
+    # Example: Registering an entity
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
+
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Unload a config entry."""
     await hass.config_entries.async_forward_entry_unload(entry, "sensor")
     hass.data[DOMAIN].pop(entry.entry_id)
     return True
--- custom_components/homeassistant_todo_important/config_flow.py
+++ custom_components/homeassistant_todo_important/config_flow.py
@@ -1,68 +1,35 @@
 import logging
-import requests
-import voluptuous as vol
 from homeassistant import config_entries
-from homeassistant.core import callback
-from .const import DOMAIN, CONF_CLIENT_ID, CONF_CLIENT_SECRET, CONF_ACCESS_TOKEN, CONF_REFRESH_TOKEN
+from homeassistant.helpers import config_entry_oauth2_flow
+
+from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
-class MicrosoftToDoConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for Microsoft To Do integration."""
+class MicrosoftToDoOAuth2FlowHandler(config_entry_oauth2_flow.AbstractOAuth2FlowHandler, domain=DOMAIN):
+    """Config flow to handle Microsoft To Do OAuth2 authentication."""
 
-    VERSION = 1
+    DOMAIN = DOMAIN
+
+    @property
+    def logger(self):
+        return _LOGGER
 
     async def async_step_user(self, user_input=None):
-        """Handle the initial step to get client credentials and tokens."""
-        errors = None
-        if user_input is not None:
-            self.client_id = user_input[CONF_CLIENT_ID]
-            self.client_secret = user_input[CONF_CLIENT_SECRET]
-            self.access_token = user_input[CONF_ACCESS_TOKEN]
-            self.refresh_token = user_input[CONF_REFRESH_TOKEN]
+        """Initial step to start OAuth2 login."""
+        return await self.async_step_auth()
 
-            return self.async_create_entry(
-                title="Microsoft To Do",
-                data={
-                    CONF_CLIENT_ID: self.client_id,
-                    CONF_CLIENT_SECRET: self.client_secret,
-                    CONF_ACCESS_TOKEN: self.access_token,
-                    CONF_REFRESH_TOKEN: self.refresh_token,
-                }
-            )
+    async def async_step_auth(self):
+        """Handle the authentication step."""
+        self.logger.debug("Redirecting user to Microsoft login.")
+        return await self.async_oauth_create_entry()
 
-        return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema({
-                vol.Required(CONF_CLIENT_ID): str,
-                vol.Required(CONF_CLIENT_SECRET): str,
-                vol.Required(CONF_ACCESS_TOKEN): str,
-                vol.Required(CONF_REFRESH_TOKEN): str,
-            }),
-            errors=errors,
-        )
+    async def async_step_reauth(self, user_input=None):
+        """Handle re-authentication."""
+        self.logger.debug("Re-authenticating user.")
+        return await self.async_step_auth()
 
-    @staticmethod
-    @callback
-    def async_get_options_flow(config_entry):
-        return MicrosoftToDoOptionsFlowHandler(config_entry)
-
-
-class MicrosoftToDoOptionsFlowHandler(config_entries.OptionsFlow):
-    """Handle options for the Microsoft To Do integration."""
-
-    def __init__(self, config_entry):
-        """Initialize the options flow."""
-        self.config_entry = config_entry
-
-    async def async_step_init(self, user_input=None):
-        """Manage the options for the integration."""
-        if user_input is not None:
-            return self.async_create_entry(title="", data=user_input)
-
-        return self.async_show_form(
-            step_id="init",
-            data_schema=vol.Schema({
-                vol.Required("polling_interval", default=5): int,
-            }),
-        )
+    async def async_oauth_create_entry(self):
+        """Create an entry for the OAuth authentication."""
+        self.logger.debug("Creating OAuth entry.")
+        return await self.async_create_entry(title="Microsoft To Do", data=self.token)
